5244d040c1d557e9fec75c8c44a59f3bd57c6402
goodmirow-1 <57578975+goodmirow-1@users.noreply.github.com>
2024-03-18 11:13:00 +0900

    refactoring modify

--- src/point/point.controller.ts
+++ src/point/point.controller.ts
@@ -1,124 +1,159 @@
-import { BadRequestException, Body, Controller, Get, NotFoundException, Param, Patch, ValidationPipe } from "@nestjs/common";
-import { PointHistory, TransactionType, UserPoint } from "./point.model";
-import { UserPointTable } from "src/database/userpoint.table";
-import { PointHistoryTable } from "src/database/pointhistory.table";
-import { PointBody as PointDto } from "./point.dto";
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  ValidationPipe,
+} from '@nestjs/common';
+import { PointHistory, TransactionType, UserPoint } from './point.model';
+import { UserPointTable } from 'src/database/userpoint.table';
+import { PointHistoryTable } from 'src/database/pointhistory.table';
+import { PointBody as PointDto } from './point.dto';
 
 @Controller('/point')
 export class PointController {
-    //동시성 제어를 위한 userid와 promise 맵
-    taskQueues: Map<number, Promise<any>> = new Map();
+  //동시성 제어를 위한 userid와 promise 맵
+  taskQueues: Map<number, Promise<any>> = new Map();
 
-    constructor(
-        private readonly userDb: UserPointTable,
-        private readonly historyDb: PointHistoryTable,
-    ) {}
+  constructor(
+    private readonly userDb: UserPointTable,
+    private readonly historyDb: PointHistoryTable,
+  ) {}
 
-    enqueueTask(userId: number, task: () => Promise<void>): Promise<void> {
-        const previousTask = this.taskQueues.get(userId) || Promise.resolve();
-        const nextTask = async () => {
-            try {
-                await previousTask;
-            } catch (error) {
-                // 실패한 테스크는 거른다.
-                console.error("Previous task failed", error);
-            }
-            return task();
-        };
-    
-        const taskPromise = nextTask();
-        this.taskQueues.set(userId, taskPromise);
-    
-        //완료되거나 실패한 queue의 경우 삭제
-        taskPromise.then(
-            () => this.taskQueues.delete(userId),
-            () => this.taskQueues.delete(userId)
+  enqueueTask(userId: number, task: () => Promise<void>): Promise<void> {
+    const previousTask = this.taskQueues.get(userId) || Promise.resolve();
+    const nextTask = async () => {
+      try {
+        await previousTask;
+      } catch (error) {
+        // 실패한 테스크는 거른다.
+        console.error('Previous task failed', error);
+      }
+      return task();
+    };
+
+    const taskPromise = nextTask();
+    this.taskQueues.set(userId, taskPromise);
+
+    //완료되거나 실패한 queue의 경우 삭제
+    taskPromise.then(
+      () => this.taskQueues.delete(userId),
+      () => this.taskQueues.delete(userId),
+    );
+
+    return taskPromise;
+  }
+
+  /**
+   * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
+   */
+  @Get(':id')
+  async point(@Param('id') id): Promise<UserPoint> {
+    const userId = Number.parseInt(id);
+    return {
+      id: userId,
+      point: (await this.userDb.selectById(userId)).point,
+      updateMillis: Date.now(),
+    };
+  }
+
+  /**
+   * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
+   */
+  @Get(':id/histories')
+  async history(@Param('id') id): Promise<PointHistory[]> {
+    const userId = Number.parseInt(id);
+    return await this.historyDb.selectAllByUserId(userId);
+  }
+
+  /**
+   * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
+   */
+  @Patch(':id/charge')
+  async charge(
+    @Param('id') id,
+    @Body(ValidationPipe) pointDto: PointDto,
+  ): Promise<UserPoint> {
+    const userId = Number.parseInt(id);
+    const amount = pointDto.amount;
+
+    //포인트 충전할때 유의해야할것
+    //(1) 충전하려는 포인트가 마이너스(예외의 값)인가
+    //(2) 충전하려는 포인트가 너무 크진 않은가
+    if (amount < 0 || amount > 100000)
+      throw new BadRequestException('올바르지 않은 포인트 입니다.');
+
+    await this.enqueueTask(userId, async () => {
+      const currentUserPoint = await this.userDb.selectById(userId);
+      await this.modifyUserPoint(
+        currentUserPoint,
+        amount,
+        TransactionType.CHARGE,
+      );
+    });
+
+    const updatedPoint = await this.userDb.selectById(userId);
+    return {
+      id: updatedPoint.id,
+      point: updatedPoint.point,
+      updateMillis: Date.now(),
+    };
+  }
+
+  /**
+   * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
+   */
+  @Patch(':id/use')
+  async use(
+    @Param('id') id,
+    @Body(ValidationPipe) pointDto: PointDto,
+  ): Promise<UserPoint> {
+    const userId = Number.parseInt(id);
+    const amount = pointDto.amount;
+
+    //포인트 사용할때 유의해야할것
+    //(1) 사용하려는 포인트가 마이너스(예외의 값)인가
+    //(2) 사용하려는 포인트가 너무 크진 않은가
+    //(3) 사용하려는 포인트가 유저가 가지고 있는 포인트보다 큰가
+    if (amount < 0 || amount > 100000)
+      throw new BadRequestException('올바르지 않은 포인트 입니다.');
+
+    await this.enqueueTask(userId, async () => {
+      // 사용자의 포인트 조회
+      const currentUserPoint = await this.userDb.selectById(userId);
+      if (currentUserPoint.point < amount)
+        throw new BadRequestException(
+          '사용하려는 포인트가 보유 포인트보다 큽니다.',
         );
-    
-        return taskPromise;
-    }
 
-    /**
-     * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
-     */
-    @Get(':id')
-    async point(@Param('id') id): Promise<UserPoint> {
-        const userId = Number.parseInt(id)
-        return {id: userId, point: (await this.userDb.selectById(userId)).point, updateMillis: Date.now()} 
-    }
+      await this.modifyUserPoint(currentUserPoint, amount, TransactionType.USE);
+    });
 
-    /**
-     * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
-     */
-    @Get(':id/histories')
-    async history(@Param('id') id): Promise<PointHistory[]> {
-        const userId = Number.parseInt(id)
-        return await this.historyDb.selectAllByUserId(userId);
-    }
+    const updatedPoint = await this.userDb.selectById(userId);
+    return {
+      id: updatedPoint.id,
+      point: updatedPoint.point,
+      updateMillis: Date.now(),
+    };
+  }
 
-    /**
-     * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
-     */
-    @Patch(':id/charge')
-    async charge(
-        @Param('id') id,
-        @Body(ValidationPipe) pointDto: PointDto,
-    ): Promise<UserPoint> {
-        const userId = Number.parseInt(id)
-        const amount = pointDto.amount
+  async modifyUserPoint(
+    currentUserPoint: UserPoint,
+    amount: number,
+    type: TransactionType,
+  ) {
+    // 사용자 포인트 적용
+    const updatedPoint =
+      type == TransactionType.USE
+        ? currentUserPoint.point - amount
+        : currentUserPoint.point + amount;
 
-        //포인트 충전할때 유의해야할것
-        //(1) 충전하려는 포인트가 마이너스(예외의 값)인가
-        //(2) 충전하려는 포인트가 너무 크진 않은가
-        if(amount < 0 || amount > 100000) throw new BadRequestException('올바르지 않은 포인트 입니다.');
+    // 사용자 포인트 갱신
+    await this.userDb.insertOrUpdate(currentUserPoint.id, updatedPoint);
 
-        await this.enqueueTask(userId, async () => {
-            const currentUserPoint = await this.userDb.selectById(userId);
-            await this.modifyUserPoint(currentUserPoint,amount,TransactionType.CHARGE);
-        });
-
-
-        const updatedPoint = await this.userDb.selectById(userId);
-        return {id: updatedPoint.id, point: updatedPoint.point, updateMillis: Date.now()}
-    }
-
-    /**
-     * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
-     */
-    @Patch(':id/use')
-    async use(
-        @Param('id') id,
-        @Body(ValidationPipe) pointDto: PointDto,
-    ): Promise<UserPoint> {
-        const userId = Number.parseInt(id)
-        const amount = pointDto.amount
-
-        //포인트 사용할때 유의해야할것
-        //(1) 사용하려는 포인트가 마이너스(예외의 값)인가
-        //(2) 사용하려는 포인트가 너무 크진 않은가
-        //(3) 사용하려는 포인트가 유저가 가지고 있는 포인트보다 큰가
-        if(amount < 0 || amount > 100000) throw new BadRequestException('올바르지 않은 포인트 입니다.');
-
-        await this.enqueueTask(userId, async () => {
-            // 사용자의 포인트 조회
-            const currentUserPoint = await this.userDb.selectById(userId);
-            if(currentUserPoint.point < amount) throw new BadRequestException('사용하려는 포인트가 보유 포인트보다 큽니다.');
-
-            await this.modifyUserPoint(currentUserPoint,amount,TransactionType.USE)
-        });
-        
-        const updatedPoint = await this.userDb.selectById(userId);
-        return {id: updatedPoint.id, point: updatedPoint.point, updateMillis: Date.now()} 
-    }
-
-    async modifyUserPoint(currentUserPoint: UserPoint,amount:number,type:TransactionType){
-        // 사용자 포인트 적용
-        const updatedPoint = type == TransactionType.USE ? currentUserPoint.point - amount : currentUserPoint.point + amount;
-
-        // 사용자 포인트 갱신
-        const updatedUserPoint = await this.userDb.insertOrUpdate(currentUserPoint.id, updatedPoint);
-
-        // 로그 남기기
-        await this.historyDb.insert(currentUserPoint.id,amount,type,Date.now())
-    }
+    // 로그 남기기
+    await this.historyDb.insert(currentUserPoint.id, amount, type, Date.now());
+  }
 }
