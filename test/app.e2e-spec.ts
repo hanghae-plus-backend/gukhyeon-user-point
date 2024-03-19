fb6c387543bf836bd3382b8293f2eceb6ddd5402
goodmirow-1 <57578975+goodmirow-1@users.noreply.github.com>
2024-03-19 11:15:52 +0900

    [#1] fix e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -7,6 +7,11 @@ import { PointModule } from '../src/point/point.module'
 describe('AppController (e2e)', () => {
     let app: INestApplication
 
+    const userId = 1
+    const max = 10
+    const amount = 100
+    const tenSecond = 100000
+
     beforeEach(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [AppModule, PointModule],
@@ -16,54 +21,77 @@ describe('AppController (e2e)', () => {
         await app.init()
     })
 
-    // it('/ (PATCH) point charge is response BAD_REQUEST cause invalid amount', async () => {
-    //     const userId = 1
-    //     const amount = -100
-
-    //     const result = await request(app.getHttpServer())
-    //         .patch(`/point/${userId}/charge`)
-    //         .send({ amount })
-
-    //     expect(result.status).toBe(HttpStatus.BAD_REQUEST)
-    // })
-
-    it('/ (PATCH) point charge is success', async () => {
+    it('/ (PATCH) point charge is response BAD_REQUEST cause invalid amount', async () => {
         const userId = 1
-        const max = 10
-        const amount = 100
-
-        const requests = Array(max)
-            .fill(null)
-            .map(async () => {
-                await request(app.getHttpServer())
-                    .patch(`/point/${userId}/charge`)
-                    .send({ amount })
-                    .expect(HttpStatus.OK)
-            })
-
-        await Promise.all(requests)
+        const amount = -100
 
         const result = await request(app.getHttpServer())
-            .get(`/point/${userId}/histories`)
-            .expect(HttpStatus.OK)
+            .patch(`/point/${userId}/charge`)
+            .send({ amount })
 
-        expect(result.body.length).toEqual(max)
+        expect(result.status).toBe(HttpStatus.BAD_REQUEST)
     })
 
-    // it('/(POST) point use is response BAD_REQUEST cause exceed amount', async () => {
-    //     const userId = 1
-    //     const charge = 100
-    //     const use = 2000
+    it(
+        '/ (PATCH) point charge is response OK',
+        async () => {
+            const requests = Array(max)
+                .fill(null)
+                .map(async () => {
+                    await request(app.getHttpServer())
+                        .patch(`/point/${userId}/charge`)
+                        .send({ amount })
+                        .expect(HttpStatus.OK)
+                })
 
-    //     await request(app.getHttpServer())
-    //         .patch(`/point/${userId}/charge`)
-    //         .send({ amount: charge })
-    //         .expect(200)
+            await Promise.all(requests)
 
-    //     const result = await request(app.getHttpServer())
-    //         .patch(`/point/${userId}/use`)
-    //         .send({ amount: use })
+            const result = await request(app.getHttpServer())
+                .get(`/point/${userId}/histories`)
+                .expect(HttpStatus.OK)
 
-    //     expect(result.status).toBe(HttpStatus.BAD_REQUEST)
-    // })
+            expect(result.body.length).toEqual(max)
+        },
+        tenSecond,
+    )
+
+    it('/(POST) point use is response BAD_REQUEST cause exceed amount', async () => {
+        await request(app.getHttpServer())
+            .patch(`/point/${userId}/charge`)
+            .send({ amount })
+            .expect(HttpStatus.OK)
+
+        await request(app.getHttpServer())
+            .patch(`/point/${userId}/use`)
+            .send({ amount: 10000 })
+            .expect(HttpStatus.BAD_REQUEST)
+    })
+
+    it(
+        '/(POST) point use is response OK',
+        async () => {
+            await request(app.getHttpServer())
+                .patch(`/point/${userId}/charge`)
+                .send({ amount: 1000 })
+                .expect(HttpStatus.OK)
+
+            const requests = Array(max)
+                .fill(null)
+                .map(async () => {
+                    await request(app.getHttpServer())
+                        .patch(`/point/${userId}/use`)
+                        .send({ amount })
+                        .expect(HttpStatus.OK)
+                })
+
+            await Promise.all(requests)
+
+            const result = await request(app.getHttpServer())
+                .get(`/point/${userId}`)
+                .expect(HttpStatus.OK)
+
+            expect(result.body.id).toEqual(userId)
+        },
+        tenSecond,
+    )
 })
